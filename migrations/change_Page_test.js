cc209896596d8bccd904dbc4b62f2b7d3a47ba61
Samuel Höra <samuel.hoera@myposter.de>
2024-05-17 18:38:06 +0200

    setup for startwith

---
+++ migrations/change_Page_test.js
@@ -0,0 +1,11 @@
+/**
+ * @param  {Object} block the block to be migrated
+ * @param  {String} storyFullSlug the full slug of the containing story
+ */
+export default function (block, storyFullSlug) {
+  // Example to change a string to boolean
+  // block.test = !!(block.test)
+
+  // Example to transfer content from other field
+  // block.test = block.other_field
+}
