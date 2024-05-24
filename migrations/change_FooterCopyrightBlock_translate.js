18b58da30fb2edd0f2d8b0a480a6fdaa00d2ee4e
Maja Grohs <maja.grohs@myposter.de>
2024-05-24 13:17:17 +0200

    fix: missing t

--- migrations/change_FooterCopyrightBlock_translate.js
+++ migrations/change_FooterCopyrightBlock_translate.js
@@ -1,5 +1,5 @@
 import translateOpenAI from '../src/utils/translate-openai.js';
 
 export default async function (block) {
-  await translateOpenAI(block, 'ext');
+  await translateOpenAI(block, 'text');
 }
